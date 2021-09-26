fcc2a567e53577841abd2f7e925eb1d9efdd06aa
Stephanie Godoi <stephaniegoo99@gmail.com>
2021-09-26 13:03:05 -0300

    test: testes unitarios do useCase de negociacao

--- tests/Resistence.Tests/UseCasesTests.cs
+++ tests/Resistence.Tests/UseCasesTests.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Threading.Tasks;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
@@ -7,6 +6,8 @@ using Resistence.Interfaces.UseCases.Interfaces;
 using Resistence.Middleware;
 using Resistence.UseCases;
 using Xunit;
+using Resistence.Entities.DTOs;
+using System.Collections.Generic;
 
 namespace Resistence.Tests
 {
@@ -14,6 +15,7 @@ namespace Resistence.Tests
     {
         private IRebeldesUseCase rebeldesUseCase;
         private IReportarUseCase reportarUseCase;
+        private INegociacaoUseCase negociacaoUseCase;
 
         public UseCasesTests()
         {
@@ -26,6 +28,7 @@ namespace Resistence.Tests
             context.Database.EnsureCreated();
             this.rebeldesUseCase = new RebeldesUseCase(context);
             this.reportarUseCase = new ReportarUseCase(context);
+            this.negociacaoUseCase = new NegociacaoUseCase(context);
         }
 
         [Fact]
@@ -84,6 +87,91 @@ namespace Resistence.Tests
 
             Assert.NotNull(result);
             Assert.True(result.Latitude == "123" && result.Longitude == "456" && result.Nome == "Planeta 1");
+        }
+
+        [Fact]
+        public void Negociar_WhellCalled_WithWrongItensSumValue_ShouldThrowSomaDosItensNaoCompativelException() {
+            var inventarioUm = new Dictionary<ITEM_INVENTARIODTO, int>();
+            var inventarioDois = new Dictionary<ITEM_INVENTARIODTO, int>();
+            
+            // Na soma vai dar 4 pontos
+            inventarioUm.Add(ITEM_INVENTARIODTO.COMIDA, 4);
+
+            // Na soma vai dar 2 pontos
+            inventarioDois.Add(ITEM_INVENTARIODTO.AGUA, 1);
+            
+            var negociacaoDto = new NegociacaoDto() {
+                codigoRebeldeUm = 1,
+                codigoRebeldeDois = 2,
+                itensDeTrocaRebeldeUm = inventarioUm,
+                itensDeTrocaRebeldeDois = inventarioDois 
+            };
+
+
+            Assert.ThrowsAsync<SomaDosItensNaoCompativelException>(() => this.negociacaoUseCase.Negociar(negociacaoDto));
+        }
+
+        [Fact]
+        public void Negociar_WhellCalled_WithWrongItensQuantityInBothRebelds_ShouldThrowInconsistenciaDeItensInventarioException() {
+            var inventarioUm = new Dictionary<ITEM_INVENTARIODTO, int>();
+            var inventarioDois = new Dictionary<ITEM_INVENTARIODTO, int>();
+            
+            // A soma de pontos vai dar certo mas a quantidade vai disparar a excecao de inconsistencia. Mais itens do que realmente tem
+            inventarioUm.Add(ITEM_INVENTARIODTO.COMIDA, 100);
+
+            inventarioDois.Add(ITEM_INVENTARIODTO.AGUA, 50);
+            
+            var negociacaoDto = new NegociacaoDto() {
+                codigoRebeldeUm = 1,
+                codigoRebeldeDois = 2,
+                itensDeTrocaRebeldeUm = inventarioUm,
+                itensDeTrocaRebeldeDois = inventarioDois 
+            };
+
+
+            Assert.ThrowsAsync<InconsistenciaDeItensInventarioException>(() => this.negociacaoUseCase.Negociar(negociacaoDto));
+        }
+
+        [Fact]
+        public void Negociar_WhellCalled_WithWrongItensFromSecondRebel_ShouldThrowInconsistenciaDeItensInventarioException() {
+            var inventarioUm = new Dictionary<ITEM_INVENTARIODTO, int>();
+            var inventarioDois = new Dictionary<ITEM_INVENTARIODTO, int>();
+            
+            // A soma de pontos vai dar certo mas a quantidade vai disparar a excecao de inconsistencia. Mais itens do que realmente tem
+            inventarioUm.Add(ITEM_INVENTARIODTO.AGUA, 4);
+
+            inventarioDois.Add(ITEM_INVENTARIODTO.ARMA, 2);
+            
+            var negociacaoDto = new NegociacaoDto() {
+                codigoRebeldeUm = 1,
+                codigoRebeldeDois = 2,
+                itensDeTrocaRebeldeUm = inventarioUm,
+                itensDeTrocaRebeldeDois = inventarioDois 
+            };
+
+
+            Assert.ThrowsAsync<InconsistenciaDeItensInventarioException>(() => this.negociacaoUseCase.Negociar(negociacaoDto));
+        }
+
+        [Fact]
+        public async Task Negociar_WhellCalled_WithWrongItensSumValue_ShouldReturnTrue() {
+            var inventarioUm = new Dictionary<ITEM_INVENTARIODTO, int>();
+            var inventarioDois = new Dictionary<ITEM_INVENTARIODTO, int>();
+            
+            // A soma de pontos vai dar certo e a quantidade também. Deve retornar true
+            inventarioUm.Add(ITEM_INVENTARIODTO.COMIDA, 2);
+
+            inventarioDois.Add(ITEM_INVENTARIODTO.AGUA, 1);
+            
+            var negociacaoDto = new NegociacaoDto() {
+                codigoRebeldeUm = 1,
+                codigoRebeldeDois = 2,
+                itensDeTrocaRebeldeUm = inventarioUm,
+                itensDeTrocaRebeldeDois = inventarioDois 
+            };
+
+
+            Assert.True(await this.negociacaoUseCase.Negociar(negociacaoDto));
         }
     }
 }
